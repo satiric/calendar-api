65d530337606b5a95fd50770ba9ac8ebdcd70830
Konstantin Maruhnich <nocturneumbra@gmail.com>
2017-08-10 12:46:53 +0300

    fx const

--- config/constants.js
+++ config/constants.js
@@ -4,7 +4,7 @@
 
 
 module.exports.constants = {
-    "devUrl" : "http://lastshelter.net:1337",
+    "devUrl" : "http://vlife.dev.cleveroad.com",
     "s3id" : "AKIAIK6MMZYIGAGCRIZQ",
     "s3secret" : "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"
 };
